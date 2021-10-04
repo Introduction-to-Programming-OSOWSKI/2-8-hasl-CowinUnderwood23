35a170fd72190e4f24e5eba7a88a3247e3401927
CowinUnderwood23 <817281@mystma.org>
2021-10-04 15:18:25 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    x = 0
+    while len[w] > x:
+     
+        if w[x] == "l":
+            return True
+
+        else:
+            return False
+
+        x = x + 1
+
+hasL("letter")
